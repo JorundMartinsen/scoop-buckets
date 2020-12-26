a2b82115328f0b3349359ac7ac9e2ffb751473e8
Jørund Martinsen <joerundmartinsen@hotmail.com>
2020-12-26 21:38:07 +0100

    Create setsounddevices.ps1

---
+++ setsounddevices.ps1
@@ -0,0 +1,4 @@
+nircmdc setdefaultsounddevice "VoiceMeeter Input" 1
+nircmdc setdefaultsounddevice "VoiceMeeter Output" 1
+nircmdc setdefaultsounddevice "VoiceMeeter Aux Input" 2
+nircmdc setdefaultsounddevice "VoiceMeeter Aux Output" 2
